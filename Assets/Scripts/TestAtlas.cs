5750bfc28c9ef3b032db58c9847c45ab236a9178
igniteBoom <parkbnmki@gmail.com>
2022-09-01 19:22:27 +0900

    Plugin 및 scene fade 작업 중
    Plugin 및 scene fade 작업 중

---
+++ Assets/Scripts/TestAtlas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TestAtlas : MonoBehaviour
+{
+    [SerializeField]
+    public SpriteAtlas m_atlas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
